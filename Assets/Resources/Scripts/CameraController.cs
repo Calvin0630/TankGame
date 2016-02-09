f03079977a61c2ce87ff1260971e1a94736cc9a5
Calvin Gaspar <cg7057705@gmail.com>
2016-02-09 12:47:36 -0500

    im tired

---
+++ Assets/Resources/Scripts/CameraController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+    public Vector3 positionRelativeToPlayer;
+    public GameObject target;
+    public float rotateSpeedX;
+    public float rotateSpeedY;
+    Vector3 offset;
+    Vector3 yRotation = Vector3.zero;
+    float horizontal, vertical;
+
+    void Start() {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        offset = -positionRelativeToPlayer;
+    }
+
+    void LateUpdate() {
+
+        GetCameraDeltaMouse();
+        target.transform.Rotate(0, horizontal, 0);
+        yRotation += new Vector3(0, vertical, 0);
+
+        float desiredAngle = target.transform.eulerAngles.y;
+        Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
+        transform.position = target.transform.position - (rotation * offset);
+
+        transform.LookAt(target.transform.position + yRotation);
+
+    }
+
+    //gets necasary camera rotation from mouse
+    void GetCameraDeltaMouse() {
+        horizontal = Input.GetAxis("Mouse X") * rotateSpeedX;
+        vertical = -Input.GetAxis("Mouse Y") * rotateSpeedY;
+    }
+
+    //gets necasary camera rotation from controller
+    void GetCameraDeltaController() {
+        horizontal = Input.GetAxis("RStickX") * rotateSpeedX;
+        vertical = Input.GetAxis("RStickY") * rotateSpeedY;
+    }
+}
---
+++ Assets/Resources/Scripts/TankMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TankMovement : MonoBehaviour {
+    public float acceleration, maxSpeed;
+    Rigidbody rBody;
+	// Use this for initialization
+	void Start () {
+        rBody = GetComponent<Rigidbody>();
+        if (maxSpeed == 0 || acceleration ==0) print("TankMovement.speed == 0 || TankMovement.acceleration == 0");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        print(gameObject.transform.forward);
+        rBody.AddForce(gameObject.transform.forward * acceleration * Input.GetAxis("Vertical"));
+	}
+}
